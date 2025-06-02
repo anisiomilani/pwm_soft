357f7638f8e574958590eea87f36ce122fcc0cbe
Guilherme <guilhermemarcio.soares@gmail.com>
2025-06-02 11:49:22 -0300

    commit inicial

---
+++ CPU1_RAM/pwm_sim.d
@@ -0,0 +1,267 @@
+# FIXED
+
+pwm_sim.obj: ../pwm_sim.c
+pwm_sim.obj: C:/Users/guilh/Documents/WSC2000/pwm_sim/device/driverlib.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_memmap.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/adc.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdbool.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_ti_config.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/linkage.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdint.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_stdint40.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/stdint.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/cdefs.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_types.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_types.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_stdint.h
+pwm_sim.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_stdint.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_adc.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sysctl.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_types.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cpu.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/debug.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/asysctl.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_asysctl.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/can.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_can.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sysctl.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_nmi.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_otp.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/interrupt.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ints.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_pie.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cla.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cla.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/clb.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_clb.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cmpss.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cmpss.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cputimer.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cputimer.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dac.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dac.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dcsm.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dcsm.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dma.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dma.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ecap.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ecap.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/emif.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_emif.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_memcfg.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/epwm.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_epwm.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/eqep.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_eqep.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/flash.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_flash.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/gpio.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_gpio.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_xint.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/xbar.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_clbxbar.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_epwmxbar.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_inputxbar.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_outputxbar.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_xbar.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hrpwm.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_hrpwm.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hrpwm.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/i2c.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_i2c.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hw_reg_inclusive_terminology.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ipc.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ipc.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/mcbsp.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_mcbsp.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/memcfg.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/pin_map.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/pin_map_legacy.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sci.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sci.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sdfm.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sdfm.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/spi.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_spi.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/upp.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_upp.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/version.h
+pwm_sim.obj: C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/driver_inclusive_terminology_mapping.h
+pwm_sim.obj: C:/Users/guilh/Documents/WSC2000/pwm_sim/device/device.h
+
+../pwm_sim.c:
+
+C:/Users/guilh/Documents/WSC2000/pwm_sim/device/driverlib.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_memmap.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/adc.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdbool.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/linkage.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdint.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_stdint40.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_types.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_types.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_stdint.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_adc.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sysctl.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_types.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cpu.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/debug.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/asysctl.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_asysctl.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/can.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_can.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sysctl.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_nmi.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_otp.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/interrupt.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ints.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_pie.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cla.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cla.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/clb.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_clb.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cmpss.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cmpss.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cputimer.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cputimer.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dac.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dac.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dcsm.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dcsm.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dma.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dma.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ecap.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ecap.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/emif.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_emif.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_memcfg.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/epwm.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_epwm.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/eqep.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_eqep.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/flash.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_flash.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/gpio.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_gpio.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_xint.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/xbar.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_clbxbar.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_epwmxbar.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_inputxbar.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_outputxbar.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_xbar.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hrpwm.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_hrpwm.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hrpwm.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/i2c.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_i2c.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hw_reg_inclusive_terminology.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ipc.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ipc.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/mcbsp.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_mcbsp.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/memcfg.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/pin_map.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/pin_map_legacy.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sci.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sci.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sdfm.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sdfm.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/spi.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_spi.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/upp.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_upp.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/version.h:
+
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/driver_inclusive_terminology_mapping.h:
+
+C:/Users/guilh/Documents/WSC2000/pwm_sim/device/device.h:
+
---
+++ pwm_soft.c
@@ -0,0 +1,176 @@
+//#############################################################################
+//
+// ARQUIVO:    ex1_pwm_physical_software_control.c
+//
+// TÍTULO:    Geração de PWM por Software e Configuração Simulada
+//
+//! Este exemplo gera um PWM por software e simula sua configuração via registrador.
+//! Observar o brilho do LED e variáveis no depurador do CCS.
+//
+//#############################################################################
+//
+// $Data de Lançamento: $
+// $Copyright:
+// Copyright (C) 2013-2024 Texas Instruments Incorporated - http://www.ti.com/
+//
+// Redistribuição e uso em formatos de código-fonte e binários, com ou sem
+// modificação, são permitidos desde que as seguintes condições sejam
+// atendidas:
+//
+//   As redistribuições do código-fonte devem reter o aviso de direitos autorais
+//   acima, esta lista de condições e a seguinte isenção de responsabilidade.
+//
+//   As redistribuições em formato binário devem reproduzir o aviso de direitos autorais
+//   acima, esta lista de condições e a seguinte isenção de responsabilidade na
+//   documentação e/ou outros materiais fornecidos com a distribuição.
+//
+//   Nem o nome da Texas Instruments Incorporated nem os nomes de
+//   seus colaboradores podem ser usados para endossar ou promover produtos derivados
+//   deste software sem permissão prévia por escrito.
+//
+// ESTE SOFTWARE É FORNECIDO PELOS DETENTORES DOS DIREITOS AUTORAIS E COLABORADORES
+// "AS IS" E QUAISQUER GARANTIAS EXPRESSAS OU IMPLÍCITAS, INCLUINDO, MAS NÃO
+// SE LIMITANDO A, AS GARANTIAS IMPLÍCITAS DE COMERCIALIZAÇÃO E ADEQUAÇÃO PARA
+// UM PROPÓSITO ESPECÍFICO SÃO REJEITADAS. EM NENHUM CASO O DETENTOR DOS DIREITOS AUTORAIS
+// OU COLABORADORES SERÃO RESPONSÁVEIS POR QUAISQUER DANOS DIRETOS, INDIRETOS, INCIDENTAIS,
+// ESPECIAIS, EXEMPLARES OU CONSEQUENCIAIS (INCLUINDO, MAS NÃO SE LIMITANDO A,
+// AQUISIÇÃO DE BENS OU SERVIÇOS SUBSTITUTOS; PERDA DE USO, DATA OU LUCROS;
+// OU INTERRUPÇÃO DE NEGÓCIOS) SEJA QUAL FOR A CAUSA E SOB QUALQUER TEORIA DE
+// RESPONSABILIDADE, SEJA EM CONTRATO, RESPONSABILIDADE ESTRITA OU ATO ILÍCITO
+// (INCLUINDO NEGLIGÊNCIA OU OUTRO) DECORRENTE DE QUALQUER FORMA DO USO DESTE
+// SOFTWARE, MESMO SE AVISADO DA POSSIBILIDADE DE TAL DANO.
+// $
+//#############################################################################
+
+// Arquivos Incluídos
+#include "driverlib.h"
+#include "device.h"
+
+// --- Definições ---
+#define LED_GPIO_PIN        31U     // GPIO do LED2 (Azul) na LaunchPadXL
+
+#define PWM_COMPARE_MASK    0x03FFU // Máscara para bits 0-9 (valor de comparação)
+#define PWM_ENABLE_BIT      (1U << 10) // Bit 10: habilita PWM
+#define PWM_INVERT_BIT      (1U << 11) // Bit 11: inverte saída
+
+#define PWM_PERIOD_US       1000U   // Período total do PWM em microssegundos
+
+// Variáveis Globais (Observar no depurador)
+unsigned int g_pwmControlReg = 0x0000U; // Registrador de controle PWM simulado
+float g_dutyCyclePercent = 50.0F;       // Ciclo de trabalho desejado (0.0 a 100.0)
+unsigned int g_timeOn_us;               // Tempo LIGADO (LED ON)
+unsigned int g_timeOff_us;              // Tempo DESLIGADO (LED OFF)
+
+// Protótipos de Funções
+void initSystemPeripherals(void);
+void initLEDGPIO(void);
+void enablePWM(void);
+void disablePWM(void);
+void calculatePWMOnOffTimes(unsigned int compareValue);
+unsigned int calculateCompareValueFromDutyCycle(float dutyCycle);
+void setPWMDutyCycleAndRegister(float dutyCycle);
+void generateSoftwarePWM(void);
+
+// Função Principal
+void main(void)
+{
+    initSystemPeripherals();
+    initLEDGPIO();
+
+    // Configura o ciclo de trabalho inicial e o registrador simulado
+    setPWMDutyCycleAndRegister(g_dutyCyclePercent);
+
+    // Loop infinito para gerar o PWM
+    for(;;)
+    {
+        setPWMDutyCycleAndRegister(g_dutyCyclePercent);
+        generateSoftwarePWM();
+    }
+}
+
+// Implementações de Funções
+
+void initSystemPeripherals(void)
+{
+    Device_init();
+    Device_initGPIO();
+    enablePWM();
+    Interrupt_initModule();
+    Interrupt_initVectorTable();
+    EINT; // Habilita Interrupções Globais
+    ERTM; // Habilita Depuração em Tempo Real
+}
+
+void initLEDGPIO(void)
+{
+    GPIO_setPadConfig(LED_GPIO_PIN, GPIO_PIN_TYPE_STD);
+    GPIO_setDirectionMode(LED_GPIO_PIN, GPIO_DIR_MODE_OUT);
+    GPIO_writePin(LED_GPIO_PIN, 1); // LED inicia desligado (ativo baixo)
+}
+
+void enablePWM(void)
+{
+    g_pwmControlReg = g_pwmControlReg | PWM_ENABLE_BIT;
+}
+
+void disablePWM(void)
+{
+    g_pwmControlReg = g_pwmControlReg & (~PWM_ENABLE_BIT);
+}
+
+// Calcula tempos ON/OFF a partir do valor de comparação do registrador.
+void calculatePWMOnOffTimes(unsigned int compareValue)
+{
+    g_timeOn_us = compareValue;
+    g_timeOff_us = PWM_PERIOD_US - g_timeOn_us;
+}
+
+// Converte ciclo de trabalho (%) para valor de comparação (0 a PWM_PERIOD_US).
+unsigned int calculateCompareValueFromDutyCycle(float dutyCycle)
+{
+    if (dutyCycle < 0.0F) dutyCycle = 0.0F;
+    else if (dutyCycle > 100.0F) dutyCycle = 100.0F;
+    return (unsigned int)((dutyCycle / 100.0F) * PWM_PERIOD_US);
+}
+
+// Configura ciclo de trabalho e atualiza registrador simulado e tempos ON/OFF.
+void setPWMDutyCycleAndRegister(float dutyCycle)
+{
+    g_dutyCyclePercent = dutyCycle;
+
+    unsigned int compareVal = calculateCompareValueFromDutyCycle(dutyCycle);
+
+    unsigned int currentConfigBits = g_pwmControlReg & (~PWM_COMPARE_MASK);
+    g_pwmControlReg = currentConfigBits | (compareVal & PWM_COMPARE_MASK);
+
+    calculatePWMOnOffTimes(compareVal);
+}
+
+// Gera um ciclo da onda PWM por software no pino do LED.
+void generateSoftwarePWM(void)
+{
+    if ((g_pwmControlReg & PWM_ENABLE_BIT) != 0U) // Se PWM habilitado
+    {
+        if ((g_pwmControlReg & PWM_INVERT_BIT) != 0U) // Lógica INVERTIDA
+        {
+            GPIO_writePin(LED_GPIO_PIN, 0); // Pino HIGH (LED OFF)
+            DEVICE_DELAY_US(g_timeOn_us);
+
+            GPIO_writePin(LED_GPIO_PIN, 1); // Pino LOW (LED ON)
+            DEVICE_DELAY_US(g_timeOff_us);
+        }
+        else // Lógica NORMAL
+        {
+            GPIO_writePin(LED_GPIO_PIN, 0); // Pino LOW (LED ON)
+            DEVICE_DELAY_US(g_timeOn_us);
+
+            GPIO_writePin(LED_GPIO_PIN, 1); // Pino HIGH (LED OFF)
+            DEVICE_DELAY_US(g_timeOff_us);
+        }
+    }
+    else // PWM desabilitado
+    {
+        GPIO_writePin(LED_GPIO_PIN, 1); // LED OFF
+        DEVICE_DELAY_US(PWM_PERIOD_US); // Aguarda período completo
+    }
+}
